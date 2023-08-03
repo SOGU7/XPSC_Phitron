e8d5246c26759322cbf2d2d91f11579a33b07373
Sogu7 <shougatad@gmail.com>
2023-08-04 00:05:16 +0600

    XPSC CODES

---
+++ Week-8/Day-7/CF_1791C.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define  ll  long long
+#define fastread() (ios_base::sync_with_stdio(0),cin.tie(0));
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int mod = 1e9+7;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int i = 0,j= n-1;
+    while(i<j)
+    {
+        if(s[i] == s[j])
+            break;
+        i++;
+        j--;
+    }
+    int ans = j-i+1;
+    cout<<ans<<"\n";
+    
+}
+
+int main()
+{
+    fastread();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       solve();
+    }
+     
+    return 0;
+}
